c605f9145591392ab6c27d80e5735f2f4f5cb14c
Alejandro <alejandrofigueirasx@gmail.com>
2023-08-25 10:56:27 -0400

    Agrega Componente CounterApp

--- 01-counter-app/src/index.js
+++ 01-counter-app/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 //import ReactDOM from 'react-dom'
 import ReactDOMClient from 'react-dom/client'
-import PrimeraApp from './PrimeraApp'
+import CounterApp from './CounterApp'
 import './index.css';
 
 const divRoot = document.querySelector('#root')
 
 // Nuevo After React18
 const root = ReactDOMClient.createRoot(divRoot); // createRoot(container!) if you use TypeScript
-root.render(<PrimeraApp saludo="Hola Mundo" subtitulo="Hola buenas" />);
+root.render(<CounterApp value={10} />);
