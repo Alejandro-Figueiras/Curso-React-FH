f605db25f4c45c9bfbda766098c02ea15041fe69
Alejandro <alejandrofigueirasx@gmail.com>
2023-08-27 11:19:36 -0400

    Agrega evento onClick

--- 01-counter-app/src/CounterApp.js
+++ 01-counter-app/src/CounterApp.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 const CounterApp = ({value}) => {
+
+    const handleAdd = e => {
+        console.log("+1")
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+            <button id='botonclick' onClick={handleAdd}>+1</button>
         </>
     )
 }
