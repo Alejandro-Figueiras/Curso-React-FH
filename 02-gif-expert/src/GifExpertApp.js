5693e428a01e3ae262eb4d92793e682dd7d6f115
Alejandro <alejandrofigueirasx@gmail.com>
2023-10-08 21:16:05 -0400

    Agrega ejemplo categorias

--- 02-gif-expert/src/GifExpertApp.js
+++ 02-gif-expert/src/GifExpertApp.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const GifExpertApp = () =>{
+export const GifExpertApp = () => {
+    //const categories = 
+    const [categories, setCategories] = useState(['One Punch', 'Samurai', 'Dragon Ball'])
+
+    const handleAdd = () => {
+        setCategories([...categories, 'Naruto']);
+    }
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr/>
+
+            <button onClick={handleAdd}>Agregar</button>
+            <ol>
+                {
+                    categories.map(cat => <li key={cat} >{cat}</li>)
+                }
+            </ol>
         </>
     )
 }
