765d5a487d4fef04fd3da53554b4f386e9315671
Alejandro <alejandrofigueirasx@gmail.com>
2025-07-14 15:32:12 -0400

    Mueve las bases de javascript a un directorio separado

--- old-01-counter-app/src/CounterApp.js
+++ old-01-counter-app/src/CounterApp.js
@@ -3,25 +3,25 @@ import PropTypes from 'prop-types'
 
 const CounterApp = () => {
 
-    const [ counter, setCounter ] = useState(0)
+	const [counter, setCounter] = useState(0)
 
-    const handleAdd = e => setCounter(counter + 1)
-    const handleSubstract = e => setCounter(counter - 1)
-    const handleReset = e => setCounter(0)
+	const handleAdd = e => setCounter(counter + 1)
+	const handleSubstract = e => setCounter(counter - 1)
+	const handleReset = e => setCounter(0)
 
-    return (
-        <>
-            <h1>CounterApp</h1>
-            <h2>{counter}</h2>
-            <button class='botonclick' onClick={handleAdd}>+1</button>
-            <button class='botonclick' onClick={handleSubstract}>-1</button>
-            <button class='botonclick' onClick={handleReset}>Reset</button>
-        </>
-    )
+	return (
+		<>
+			<h1>CounterApp</h1>
+			<h2>{counter}</h2>
+			<button class='botonclick' onClick={handleAdd}>+1</button>
+			<button class='botonclick' onClick={handleSubstract}>-1</button>
+			<button class='botonclick' onClick={handleReset}>Reset</button>
+		</>
+	)
 }
 
 CounterApp.propTypes = {
-    value: PropTypes.number
+	value: PropTypes.number
 }
 
 export default CounterApp
--- old-01-counter-app/src/PrimeraApp.js
+++ old-01-counter-app/src/PrimeraApp.js
@@ -1,23 +1,23 @@
 import React from "react";
 import PropTypes from 'prop-types'
 // Functional Component
-const PrimeraApp = ({saludo, subtitulo, ...props}) => {
-    
-    return (
-        <>
-            <h1>{saludo}</h1>
-            <p>{subtitulo}</p>
-        </>
-    )
+const PrimeraApp = ({ saludo, subtitulo, ...props }) => {
+
+  return (
+    <>
+      <h1>{saludo}</h1>
+      <p>{subtitulo}</p>
+    </>
+  )
 }
 
 PrimeraApp.propTypes = {
-    saludo: PropTypes.string.isRequired
+  saludo: PropTypes.string.isRequired
 }
 
 PrimeraApp.defaultProps = {
-    saludo: "Hola",
-    subtitulo: "Subtitulo"
+  saludo: "Hola",
+  subtitulo: "Subtitulo"
 }
 
 export default PrimeraApp;
