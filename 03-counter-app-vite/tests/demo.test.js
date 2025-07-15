dd6f2cd6c2c424eb90e4ff3de4b03e4c2ca932b0
Alejandro <alejandrofigueirasx@gmail.com>
2025-07-15 15:54:44 -0400

    03-counter-app-vite add types and describe

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,4 +1,16 @@
+// 1. Inicializacion
+// 2. Estimulo
+// 3. Observar el comportamiento
 
-test('should not fail', () => { 
-  // no throw error
+
+describe('Pruebas Demo', () => {
+
+
+  test('should not fail', () => {
+    const message1 = 'Hola Mundo'
+    const message2 = message1.trim()
+    expect(message1).toBe(message2)
+  })
+
+
 })
