f60a31b1193959f91094128df8bcef8d74759428
Alejandro <alejandrofigueirasx@gmail.com>
2025-07-16 15:44:28 -0400

    remove old counter app project

--- old-01-counter-app/src/tests/base/07-deses-arr.test.js
+++
@@ -1,8 +0,0 @@
-import { retornaArreglo } from "../../base/07-deses-arr"
-
-describe("Pruebas en desestructuración", () => {
-    test("Debe retornar un string y un numero", () => {
-        const arr = retornaArreglo();
-        expect(arr).toEqual(['ABC', 123])
-    })
-})
