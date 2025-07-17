430410af9749994c6022d3406b9d44beaa6bbb27
Alejandro <alejandrofigueirasx@gmail.com>
2025-07-17 11:36:57 -0400

    04-gif-expert-app add AddCategory component

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+import AddCategory from "./components/AddCategory"
+
 const GifExpertApp = () => {
+	const [categories, setCategories] = useState([])
+
+	const handleNewValue = (newCategory) => {
+		setCategories(c => [newCategory, ...(c.filter(c => c!= newCategory))])
+	}
+
 	return (
 		<>
 			<h1>GifExpertApp</h1>
+
+			<AddCategory onNewValue={handleNewValue} />
+
+			<ol>
+				{categories.map((c) => (
+					<li key={c}>{c}</li>
+				))}
+			</ol>
 		</>
 	)
 }
---
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+const AddCategory = ({ onNewValue }) => {
+	const [inputValue, setInputValue] = useState("")
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+
+		if (inputValue.trim().length <= 1) return
+
+		// setCategories((c) => [inputValue, ...c])
+		onNewValue(inputValue.trim())
+		setInputValue("")
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='text'
+				placeholder='Buscar Gifs'
+				value={inputValue}
+				onChange={(v) => setInputValue(v.target.value)}
+			/>
+			{/* <button type="submit">Agregar</button> */}
+		</form>
+	)
+}
+
+export default AddCategory
