440e63045b8f43ae66e5b46d2afa6718481e1fb3
Alejandro <alejandrofigueirasx@gmail.com>
2025-07-20 17:57:42 -0400

    05-hooks-app add useTodo custom hook

--- 05-hooks-app/src/08-useReducer/TodoApp.jsx
+++ 05-hooks-app/src/08-useReducer/TodoApp.jsx
@@ -1,41 +1,9 @@
-import { useEffect, useReducer } from "react"
-import { todoReducer } from "./todoReducer"
 import TodoList from "./TodoList"
 import TodoAdd from "./TodoAdd"
-
-const initialState = []
-const init = () => {
-	return JSON.parse(localStorage.getItem("todos")) || []
-}
+import { useTodo } from "../hooks/useTodo"
 
 const TodoApp = () => {
-	const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init)
-	useEffect(() => {
-		localStorage.setItem("todos", JSON.stringify(todos))
-	}, [todos])
-
-	const onNewTodo = (todo) => {
-		const action = {
-			type: "[TODO] Add Todo",
-			payload: todo,
-		}
-
-		dispatchTodo(action)
-	}
-
-	const handleDeleteTodo = (id = "") => {
-		dispatchTodo({
-			type: "[TODO] Remove Todo",
-			payload: id,
-		})
-	}
-
-  const handleToogleTodo = (id = "") => {
-		dispatchTodo({
-			type: "[TODO] Toggle Todo",
-			payload: id,
-		})
-	}
+	const {todos, handleDeleteTodo, handleToogleTodo, onNewTodo} = useTodo()
 
 	return (
 		<div>
---
+++ 05-hooks-app/src/hooks/useTodo.js
@@ -0,0 +1,44 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+const initialState = []
+const init = () => {
+	return JSON.parse(localStorage.getItem("todos")) || []
+}
+
+export const useTodo = () => {
+  const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init)
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+  const onNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    }
+
+    dispatchTodo(action)
+  }
+
+  const handleDeleteTodo = (id = "") => {
+    dispatchTodo({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    })
+  }
+
+  const handleToogleTodo = (id = "") => {
+    dispatchTodo({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    })
+  }
+
+  return {
+    todos,
+    onNewTodo,
+    handleDeleteTodo,
+    handleToogleTodo
+  }
+}
