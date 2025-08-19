38e6288c2c70d47e952670ad1d588f46ab2538db
Alejandro <alejandrofigueirasx@gmail.com>
2025-08-19 19:28:08 -0400

    07-heroes-spa add navbar

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -3,17 +3,22 @@ import HeroesApp from "../HeroesApp"
 import MarvelPage from "../heroes/pages/MarvelPage"
 import DcPage from "../heroes/pages/DcPage"
 import LoginPage from "../auth/pages/LoginPage"
+import { Navbar } from "../ui/components/Navbar"
 
 const AppRouter = () => {
-  return (
-    <Routes>
-      <Route path='marvel' element={<MarvelPage />} />
-      <Route path='dc' element={<DcPage />} />
+	return (
+		<>
+			<Navbar />
 
-      <Route path='login' element={<LoginPage />} />
-      <Route path="/*" element={<Navigate to='/marvel' />} />
-    </Routes>
-  )
+			<Routes>
+				<Route path='marvel' element={<MarvelPage />} />
+				<Route path='dc' element={<DcPage />} />
+
+				<Route path='login' element={<LoginPage />} />
+				<Route path='/*' element={<Navigate to='/marvel' />} />
+			</Routes>
+		</>
+	)
 }
 
 export default AppRouter
