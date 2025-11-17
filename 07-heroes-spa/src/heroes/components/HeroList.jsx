d59be54a74daccb890a508c0ed70d49d93cfb59c
Alejandro <alejandrofigueirasx@gmail.com>
2025-11-16 23:43:40 -0500

    07-heroes-spa add HeroCard component

--- 07-heroes-spa/src/heroes/components/HeroList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,11 +1,12 @@
 import { getHeroesByPublisher } from '../helpers/getHeroesByPublisher'
+import HeroCard from './HeroCard'
 
 const HeroList = ({publisher}) => {
   const heroes = getHeroesByPublisher(publisher)
   return (
-    <ul>
-      {heroes.map(h => <li key={h.id}>{h.superhero}</li>)}
-    </ul>
+    <div className='row row-cols-1 row-cols-md-3 g-3 '>
+      {heroes.map(h => <HeroCard key={h.id} hero={h} />)}
+    </div>
   )
 }
 
